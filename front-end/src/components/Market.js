aed6de79757e95b307ba7afa2d41eb50f5caca02
chutting <tingting.chu@thoughtworks.com>
2020-10-23 10:55:02 +0800

    [Chu] feat: complete the shopping-cart feature

--- front-end/src/components/Market.js
+++ front-end/src/components/Market.js
@@ -1,9 +1,15 @@
 import React, {Component} from 'react';
 import imgPlaceholder from '../placeholder.png';
+import shoppingCart from '../icon/shopping-cart.png';
+import ShoppingCartPop from './ShoppingCartPop';
+import { Popover, Button } from 'antd';
+import 'antd/dist/antd.css';
+import '../style/market.css';
 
 class Market extends Component {
   state = {
-    commodities: []
+    commodities: [],
+    orders: []
   }
 
   componentDidMount() {
@@ -18,43 +24,55 @@ class Market extends Component {
     })
   }
 
+  handleAddToCart(e, commodity) {
+    console.log(commodity);
+
+    let order = this.state.orders.filter(order => order.id === commodity.id);
+    let currentOrders = [];
+
+    if (order.length === 0) {
+      currentOrders = this.state.orders;
+      commodity.count = 1;
+      currentOrders.push(commodity);
+    } else {
+      currentOrders = this.state.orders.map(order => {
+        if (order.id === commodity.id) {
+          order.count ++;
+        }
+        return order;
+      })
+    }
+    console.log(currentOrders);
+
+    this.setState({
+      orders: currentOrders
+    })
+  }
 
   render() {
+    const content = (
+      <div>
+        <ShoppingCartPop orders={this.state.orders}/>
+      </div>
+    );
+
     return <section className="market">
       {
         this.state.commodities.map((commodity, index) => {
-          return <CommodityItem item = {commodity} key = {`commodity${index}`}/>;
+          return <div className="market-item" key={`market-item${index}`}>
+            <img src={imgPlaceholder} alt={commodity.name} className="commodity-item-img"></img>
+            <p>{commodity.name}</p>
+            <p>{`单价：${commodity.price}元/${commodity.unit}`}</p>
+            <input type="submit" value = "+" className ="add-to-cart" onClick={(e) => this.handleAddToCart(e, commodity)}></input>
+          </div>;
         })
       }
+      <div>
+        <Popover content={content} title="Title" trigger="click">
+          <Button type="primary" className="shopping-cart-button"><img src={shoppingCart} alt="shopping-cart" className="shopping-cart-icon"/></Button>
+        </Popover>
+      </div>
     </section>
-  }
-}
-
-class CommodityItem extends Component {
-
-  handleAddToCart = () => {
-    fetch('http://localhost:8080/order', {
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(this.props.item),
-      method: 'POST'
-    }).then(response => { 
-      if (response.status === 201) {
-        console.log('添加成功');
-      }
-    });
-  }
-
-  render() {
-    const {name, price, unit} = this.props;
-
-    return <div className="market-item">
-      <img src={imgPlaceholder} alt={name} className="commodity-item-img"></img>
-      <p>{name}</p>
-      <p>{`单价：${price}元/${unit}`}</p>
-      <input type="submit" value = "+" className ="add-to-cart" onClick={this.handleAddToCart}></input>
-    </div>
   }
 }
 
---
+++ front-end/src/components/ShoppingCartPop.js
@@ -0,0 +1,107 @@
+import React, {Component} from 'react';
+import deleteIcon from '../icon/delete.png'
+import {Button } from 'antd';
+
+class ShoppingCartPop extends Component {
+  state = {
+    orders: this.props.orders
+  }
+
+  handleDeleteAll() {
+    this.setState({
+      orders: []
+    })
+  }
+
+  handleOrderSubmit() {
+    fetch('http://localhost:8080/orders', {
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(this.state.orders),
+      method: 'POST'
+    }).then(response => {
+      if (response.status === 201) {
+        console.log('添加成功');
+      }
+    });
+  }
+
+  handleDeleteItem(e, order) {
+    console.log(order);
+    let currentOrder = this.state.orders.filter(orderItem => orderItem.id !== order.id);
+    this.setState({
+      orders: currentOrder
+    })
+  }
+
+  render() {
+    if (this.state.orders.length === 0) {
+      return <section className="no-orders">
+        <p>暂无订单，返回商城页面继续购买</p>
+      </section>
+    }
+
+    return <section className="order">
+      <table>
+        <thead>
+        <tr>
+          <th>商品</th>
+          <th>数量</th>
+          <th></th>
+        </tr>
+        </thead>
+        <tbody>
+        {
+          this.state.orders.map((order, index) => {
+            return <tr key={`order${index}`}>
+              <td>{order.name}</td>
+              <td>
+                <Button shape="circle" size="small">-</Button>
+                {order.count}
+                <Button shape="circle" size="small">+</Button>
+              </td>
+              <td><Button type="text" onClick={(e) => {this.handleDeleteItem(e, order)}}><img src={deleteIcon} alt="deleteIcon" className="delete-icon" /></Button></td>
+            </tr>
+          })
+        }
+        </tbody>
+      </table>
+      <div>
+        <Button type="primary" onClick={() => this.handleOrderSubmit()}>立即下单</Button>
+        <Button type="text" onClick={() => this.handleDeleteAll()}>清空</Button>
+      </div>
+    </section>
+  }
+}
+
+// class ShoppingCartItem extends Component {
+//
+//   handleDeleteOrder = () => {
+//     fetch('http://localhost:8080/order', {
+//       headers: {
+//         'Content-Type': 'application/json'
+//       },
+//       body: JSON.stringify(this.props.item.commodity),
+//       method: 'DELETE'
+//     }).then(response => {
+//       if (response.status === 204) {
+//         window.location.reload();
+//       }
+//     });
+//   }
+//
+//   render() {
+//     return <tr>
+//       <td>{this.props.item.commodity.name}</td>
+//       <td>
+//         <Button shape="circle" size="small">-</Button>
+//         {this.props.item.count}
+//         <Button shape="circle" size="small">+</Button>
+//       </td>
+//       <td><Button type="text" onClick={this.handleDeleteOrder}><img src={deleteIcon} alt="deleteIcon" className="delete-icon" /></Button></td>
+//     </tr>
+//   }
+// }
+
+export default ShoppingCartPop;
